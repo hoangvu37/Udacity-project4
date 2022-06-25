2b7f83c24e23c2636789360064200b7733d6246c
Hoangvu <hoangvu37>
2022-06-25 19:03:16 +0700

    app.run()

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -131,4 +131,4 @@ if __name__ == "__main__":
     # comment line below when deploying to VMSS
     #app.run() # local
     # uncomment the line below before deployment to VMSS
-    app.run(host='0.0.0.0', threaded=True, debug=false) # remote
+    app.run() 
